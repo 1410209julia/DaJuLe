1badad7f236ceba952c6298d1c47f9521b4e3cee
1410209julia <1410209@stud.hs-mannheim.de>
2015-05-15 20:02:57 +0200

    Aufgabe 2 zahlen funktionieren nicht nicht Julia
    Probleme bei der Prüfung von ISBN, Auflage; Jahr

--- Meilenstein3/book_entry.js
+++ Meilenstein3/book_entry.js
@@ -1,45 +1,84 @@
-/**
- * Created by Julia Goehring on 06.05.2015.
- */
+function pruefen(form, vorname, nachname, titel, autor, isbn, jahr, auflage) {
+    /*
+     * Bei einer weiteren Syntaxpruefung werden alle zuvor rot markierten Felder
+     * auf den Initialzustand zurueck gesetzt.
+     */
 
-function pruefen () {
-        var a = document.Formular.vorname.value;
-        if (typeof(a) == String){
-            alert(ungültig);
-            return false;
+    var falsch = false;
+
+    document.getElementById(vorname.id).style.borderColor = "";
+    document.getElementById(nachname.id).style.borderColor = "";
+    document.getElementById(autor.id).style.borderColor = "";
+    document.getElementById(isbn.id).style.borderColor="";
+    document.getElementById(jahr.id).style.borderColor="";
+    document.getElementById(auflage.id).style.borderColor="";
+
+
+    pruefer = /^[\sa-zA-ZßäöüÄÖÜ]+$/; //Ueberpueft den angegebenen Vornamen auf korrekte Syntax
+    if(!pruefer.test(vorname.value)) {
+        if(falsch == false){
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+            falsch = true;
         }
-        alert("Bitte Ihren Namen eingeben!");
-        document.Formular.User.focus();
+        document.getElementById(vorname.id).style.borderColor = "red";
 
+    }
 
-    if (document.Formular.Ort.value == "") {
-        alert("Bitte Ihren Wohnort eingeben!");
-        document.Formular.Ort.focus();
-        return false;
+    if(!pruefer.test(nachname.value)) { //Ueberpueft den angegebenen Nachnamen auf korrekte Syntax
+        if(falsch == false){
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+            falsch = true;
+        }
+        document.getElementById(nachname.id).style.borderColor = "red";
+
     }
-    if (document.Formular.Mail.value == "") {
-        alert("Bitte Ihre E-Mail-Adresse eingeben!");
-        document.Formular.Mail.focus();
-        return false;
+
+    if(!pruefer.test(autor.value)) { //Ueberpueft den angegebenen Autor auf korrekte Syntax
+        if(falsch == false){
+            alert("Einige Eingaben sind fehlerhaft. Bitte %FCberpr%FCfen Sie ihre Eingaben");
+            falsch = true;
+        }
+        document.getElementById(autor.id).style.borderColor = "red";
+
     }
-    if (document.Formular.Mail.value.indexOf("@") == -1) {
-        alert("Keine E-Mail-Adresse!");
-        document.Formular.Mail.focus();
-        return false;
+
+    if(titel.value == "") { //Ueberpueft den angegebenen Titel auf korrekte Syntax
+        if(falsch == false){
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+            falsch = true;
+        }
+
+        document.getElementById(titel.id).style.borderColor = "red";
     }
-    if (document.Formular.Alter.value == "") {
-        alert("Bitte Ihr Alter eingeben!");
-        document.Formular.Alter.focus();
-        return false;
+
+    pruefer = /^[0-9]+$/;
+    if((!regex.test(isbn.value)) || (isbn.value.length > 13)) { //Ueberpueft die angegebene ISBN auf korrekte Syntax
+        if(falsch == false){
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+            falsch = true;
+        }
+
+        document.getElementById(isbn.id).style.borderColor = "red";
     }
-    var chkZ = 1;
-    for (i = 0; i < document.Formular.Alter.value.length; ++i)
-        if (document.Formular.Alter.value.charAt(i) < "0" ||
-            document.Formular.Alter.value.charAt(i) > "9")
-            chkZ = -1;
-    if (chkZ == -1) {
-        alert("Altersangabe keine Zahl!");
-        document.Formular.Alter.focus();
-        return false;
+
+    aktuell = new Date().getFullYear();  //speichert das Jahr in einer Variable
+    if((!pruefer.test(jahr.value)) || jahr.value<0 || jahr.value > aktuell) { //Ueberprueft das angegebene Jahr auf korrekte Syntax
+        if(fehler == false){
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+            falsch = true;
+
+        }
+        document.getElementById(jahr.id).style.borderColor = "red";
     }
+
+    if(!pruefer.test(auflage.value) || auflage.value < 0) { //Ueberprueft, ob die Auflage den Syntaxregeln entspricht
+        if(falsch == false){
+            alert("Einige Eingaben sind fehlerhaft. Bitte überprüfen Sie ihre Eingaben");
+            falsch = true;
+        }
+
+        document.getElementById(auflage.id).style.borderColor = "red";
+    }
+
+    return !falsch;
 }
